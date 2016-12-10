113b32f125ce4406a509b2d8430c2567e04f3685
Haihui <harveyleo.lhh@gmail.com>
2016-12-11 00:03:50 +0800

    Support section query are return all relevant normalized sections

--- dsp_search/urls.py
+++ dsp_search/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.view_home_page, name='home_page'),
-    url(r'^results/', views.view_results_page, name='results_page'),
-    url(r'^search/', views.SectionSearchView.as_view(), name='section_search_view'),
+    url(r'^results/', views.SectionSearchView.as_view(), name='results_page'),
+    # url(r'^results/', views.view_search_results, name='results_page'),
+    # url(r'^search/', views.SectionSearchView.as_view(), name='section_search_view'),
 ]
--- dsp_search/views.py
+++ dsp_search/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from haystack.generic_views import SearchView
-
+from .forms import SectionSearchForm
 
 def view_home_page(request):
-    return render(request, 'dsp_search/home_page.html')
+    form = SectionSearchForm()
+    return render(request, 'dsp_search/home_page.html', {'search_form': form})
 
 
-def view_results_page(request):
-    return render(request, 'dsp_search/results_page.html')
+# def view_search_results(request):
+#     if request.GET.get('q'):
+#
+#     return render(request, 'dsp_search/results_page.html')
 
-
-# Create your views here.
 class SectionSearchView(SearchView):
-    template_name = 'dsp_search/search.html'
+    template_name = 'dsp_search/results_page.html'
+    form_class = SectionSearchForm
+    form_name = 'search_form'
