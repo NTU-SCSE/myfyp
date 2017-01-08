00ba143bd8bcf12c6fc2b49543c2ef5f93b1d53a
Haihui <harveyleo.lhh@gmail.com>
2017-01-08 11:19:22 +0800

    Serve pdf files from media folder

--- dsp_search/urls.py
+++ dsp_search/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.view_home_page, name='home_page'),
     url(r'^results/', views.SectionSearchView.as_view(), name='results_page'),
     url(r'^details/(?P<book>[0-9]+)/(?P<section>[0-9]+)/$', views.SectionDetailsView.as_view(), name='details_page'),
-    url(r'^pdf/(?P<book>[0-9]+)/(?P<start>[0-9]+)/(?P<end>[0-9]+)/$', views.PDFView.as_view(), name='pdf_viewer'),
+    url(r'^pdf/(?P<book>[0-9]+)/(?P<section>[0-9]+)/$', views.PDFView.as_view(), name='pdf_viewer'),
 ]
--- dsp_search/views.py
+++ dsp_search/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from haystack.generic_views import SearchView
 from django.views.generic.base import TemplateView
-from .forms import SectionSearchForm
-from pdf_client import config
-from pdf_client.api import section
-import json, requests, os
 from django.conf import settings
+from haystack.generic_views import SearchView
+from .forms import SectionSearchForm
+import os
+
 
 def view_home_page(request):
     form = SectionSearchForm()
@@ -31,58 +30,19 @@ class SectionDetailsView(SearchView):
     template_name = 'dsp_search/details_page.html'
     form_class = SectionSearchForm
     form_name = 'search_form'
-    config_file = 'dsp_search/config.json'
-
-    def __init__(self):
-        super(SectionDetailsView, self).__init__()
-        config.load_from_file(self.config_file)
-
-    def locate_section(self):
-        section_id = self.kwargs['section']
-        section_detail = section.Detail(section_id).execute()
-        next_section_detail = section.Detail(section_detail['next']).execute()
-        return section_detail['book'], section_detail['page'], next_section_detail['page']
 
     def get_context_data(self, *args, **kwargs):
         context = super(SectionDetailsView, self).get_context_data(*args, **kwargs)
-        location = self.locate_section()
-        context.update({'book_id': location[0], 'start_page': location[1], 'end_page': location[2]})
+        context.update({'book_id': self.kwargs['book'], 'section_id': self.kwargs['section']})
         return context
 
 
 class PDFView(TemplateView):
     template_name = 'dsp_search/pdf_viewer.html'
-    config_file = 'dsp_search/config.json'
-
-    def get_config(self):
-        with open(self.config_file, 'r') as file:
-            config = json.loads(file.read().replace('\n', ''))
-        return config
-
-    def send_request(self, book_id, start_page, end_page):
-        config = self.get_config()
-        url = "{base}book/read/{book_id}/{start}/{end}/".format(base=config['base_url'],
-                                                                book_id=book_id,
-                                                                start=start_page,
-                                                                end=end_page)
-        response = requests.get(url, stream=True, auth=(config['auth_args'][0], config['auth_args'][1]))
-        return response
-
-    def save_pdf(self, book_id, start_page, end_page):
-        response = self.send_request(book_id, start_page, end_page)
-        filename = "{book_id}_{start}_{end}.pdf".format(book_id=book_id,
-                                                        start=start_page,
-                                                        end=end_page)
-        filepath = os.path.join(settings.MEDIA_ROOT, filename)
-        with open(filepath, 'wb+') as file:
-            for chunk in response.iter_content(chunk_size=1024):
-                file.write(chunk)
-
-        file_url = os.path.join(settings.MEDIA_URL, filename)
-        return file_url
 
     def get_context_data(self, *args, **kwargs):
         context = super(PDFView, self).get_context_data(*args, **kwargs)
-        file_url = self.save_pdf(self.kwargs['book'], self.kwargs['start'], self.kwargs['end'])
-        context.update({'pdf_url': file_url})
+        section_url = os.path.join(settings.MEDIA_URL, 'sections/{0}/{1}.pdf'.format(self.kwargs['book'], self.kwargs['section']))
+        context.update({'pdf_url': section_url})
         return context
+
